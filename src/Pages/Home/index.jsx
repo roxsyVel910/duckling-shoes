5f770c115cded3b8375f84e4143d13a715cb0722
Roxsy <roxsy.dj@gmail.com>
2023-05-04 18:24:41 -0500

    create component card and style with talwin

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Layout from '../../Components/Layout'
+import Card from '../../Components/Card'
 
 function Home() {
   return (
     <Layout>
         Home
+        <Card/>
     </Layout>
      
   )
