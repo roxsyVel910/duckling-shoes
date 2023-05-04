d9c0aa9a81a7f0c0fc13eb7049c9a6cf59805750
Roxsy <roxsy.dj@gmail.com>
2023-05-04 16:49:28 -0500

    install and create de routes

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
-import Home from '../Home';
-import MyAccount from '../MyAccount';
-import MyOrders from '../MyOrders';
-import MyOrder from '../MyOrder';
-import NotFound from '../NotFound';
-import SignIn from '../SignIn';
-import './App.css'
+import { BrowserRouter, useRoutes } from "react-router-dom";
+import React, { Component } from "react";
+import Home from "../Home";
+import MyAccount from "../MyAccount";
+import MyOrders from "../MyOrders";
+import MyOrder from "../MyOrder";
+import NotFound from "../NotFound";
+import SignIn from "../Signin";
+import "./App.css";
 
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/my-account", element: <MyAccount /> },
+    { path: "/my-order", element: <MyOrder /> },
+    { path: "/my-orders", element: <MyOrders /> },
+    { path: "/sign-in", element: <SignIn /> },
+    { path: "/*", element: <NotFound /> }
 
+  ]);
+  return routes;
+};
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Home />
-        <MyAccount/>
-        <MyOrder/>
-        <MyOrders/>
-        <NotFound/>
-        <SignIn/>
-
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
+};
 
 export default App;
