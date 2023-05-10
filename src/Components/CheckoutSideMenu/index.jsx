4a24c6b98205c49ca4a5ee48a04e2ba6c00ac0b1
Roxsy <roxsy.dj@gmail.com>
2023-05-10 08:45:54 -0500

    delete product to cart

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,41 +1,47 @@
-import { useContext } from 'react'
-import { XMarkIcon } from '@heroicons/react/24/solid'
-import { ShoppingCartContext } from '../../Context';
-import OrderCard from '../../Components/OrderCard';
-import './styles.css'
+import { useContext } from "react";
+import { XMarkIcon } from "@heroicons/react/24/solid";
+import { ShoppingCartContext } from "../../Context";
+import OrderCard from "../../Components/OrderCard";
+import "./styles.css";
 
 const CheckoutSideMenu = () => {
-  const context = useContext(ShoppingCartContext)
-  console.log("CART: ", context.cartProducts);
+  const context = useContext(ShoppingCartContext);
+
+  const handleDelete = (id) => {
+    const filteredProducts = context.cartProducts.filter(product => product.id != id)
+    context.setCartProducts(filteredProducts)
+  }
+  
 
   return (
     <aside
-      className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}>
-      <div className='flex justify-between items-center p-6'>
-        <h2 className='font-medium text-xl'>My Order</h2>
+      className={`${
+        context.isCheckoutSideMenuOpen ? "flex" : "hidden"
+      } checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}
+    >
+      <div className="flex justify-between items-center p-6">
+        <h2 className="font-medium text-xl">My Order</h2>
         <div>
           <XMarkIcon
-            className='h-6 w-6 text-black cursor-pointer'
-            onClick={() => context.closeCheckoutSideMenu()}></XMarkIcon>
+            className="h-6 w-6 text-black cursor-pointer"
+            onClick={() => context.closeCheckoutSideMenu()}
+          ></XMarkIcon>
         </div>
       </div>
-      <div className='px-6 overflow-y-scroll'>
-
-      {
-        context.cartProducts.map(product => (
-            <OrderCard 
-            key = { product.id}
-            title= {product.title}
-            imageUrl = {product.images}
-            price = { product.price}
-
-            />
-
-        ))
-      }
+      <div className="px-6 overflow-y-scroll">
+        {context.cartProducts.map((product) => (
+          <OrderCard
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            imageUrl={product.images}
+            price={product.price}
+            handleDelete={handleDelete}
+          />
+        ))}
       </div>
     </aside>
-  )
-}
+  );
+};
 
-export default CheckoutSideMenu
+export default CheckoutSideMenu;
